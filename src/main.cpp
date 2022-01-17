0837e60683d186d12d581d1d83a77a20be3237ca
Artur Dzhus <asasin.lol10@gmail.com>
2022-01-17 19:22:08 +0200

    -Work

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,12 @@
 #include <Arduino.h>
 #include <SFE_BMP180.h>
-#include <SparkFun_APDS9960.h>                                  // Для работы с датчиком APDS-9960
-SparkFun_APDS9960 apds = SparkFun_APDS9960();                   // Определяем объект apds, экземпляр класса SparkFun_APDS9960
-                                                                //
-// Объявляем переменные:                                        //
-uint16_t lightAmbient = 0;                                      // Определяем переменную для хранения освещённости общей            в люксах
-uint16_t lightRed     = 0;                                      // Определяем переменную для хранения освещённости красного спектра в люксах
-uint16_t lightGreen   = 0;                                      // Определяем переменную для хранения освещённости зелёного спектра в люксах
-uint16_t lightBlue    = 0; 
-uint16_t general    = 0; 
+#include <SparkFun_APDS9960.h>                   
+SparkFun_APDS9960 apds = SparkFun_APDS9960();                   
+                                                                
+uint16_t lightAmbient = 0;                                     
 
 SFE_BMP180 pressure;
+const String getRasberiReqest = "giveMeData";
 
 double getPressure(){
     char status;
@@ -18,13 +14,11 @@ double getPressure(){
 
     status = pressure.startTemperature();
     if (status != 0){
-        // ожидание замера температуры
         delay(status);
         status = pressure.getTemperature(T);
         if (status != 0){
             status = pressure.startPressure(3);
             if (status != 0){
-                // ожидание замера давления
                 delay(status);
                 status = pressure.getPressure(P,T);
                 if (status != 0){
@@ -37,18 +31,17 @@ double getPressure(){
 
 
 void setup(){
-      Serial.begin(9600);                                         // Инициируем передачу данных в монитор последовательного порта на скорости 9600 бит/сек
-//  Инициируем работу датчика:                                  // 
-    if(apds.init()){                                            // Если инициализация прошла успешно, то ...
-          Serial.println("Initialization OK!");                 // Выводим сообщение об успешной инициализации датчика
-    }else{Serial.println("Initialization ERROR!");}             // Иначе, выводим сообщение об ошибке инициализации датчика
-                                                                //
-//  Разрешаем режим определения освещённости:                   //
-    if(apds.enableLightSensor(false)){                          // Если режим определения освещённости запущен (false - без прерываний на выходе INT), то ...
-          Serial.println("Start light sensor OK!");             // Выводим сообщение об успешном запуске режима определения освещённости
-    }else{Serial.println("Start light sensor ERROR!");}         // Иначе, выводим сообщение об ошибке запуска режима определения освещённости
-                                                                // Запретить режим определения освещённости можно так: bool j = apds.disableLightSensor(); // в переменную j сохранится результат выполнения функции (true/false)
-//  Ждём завершение инициализации и калибровки:                 //
+      Serial.begin(9600);                                                                       
+    if(apds.init()){                                            
+          Serial.println("Initialization OK!");                
+    }else{Serial.println("Initialization ERROR!");}             
+                                                                
+                  
+    if(apds.enableLightSensor(false)){                         
+          Serial.println("Start light sensor OK!");             
+    }else{Serial.println("Start light sensor ERROR!");}         
+                                                               
+             
     delay(500);
     Serial.begin(9600);
     Serial.println("Init"); 
@@ -58,19 +51,26 @@ void setup(){
 
 void loop(){
 
-    if(apds.readAmbientLight (lightAmbient)
-    && apds.readAmbientLight(general)
-    && apds.readRedLight     (lightRed)                         // И    прочитано значение освещённости красного спектра, и
-    && apds.readGreenLight   (lightGreen)                       // И    прочитано значение освещённости зелёного спектра, и
-    && apds.readBlueLight    (lightBlue)   ){                   // И    прочитано значение освещённости синего   спектра, то ...
-          Serial.println((String) "Ambient=" + lightAmbient + ", Red=" + lightRed + ", Green=" + lightGreen + ", Blue=" + lightBlue + "General" + general + " lx"); // Выводим все прочитанные значения
-    }else{Serial.println("Read light ERROR!");}                 // Иначе, выводим сообщение об ошибке чтения освещённости
-                                                                //           //
-    delay(4000); 
-  
-  Serial.println("Start");
-    double P;
-    P = getPressure();
-    Serial.println(P, 4); 
-    delay(4000);
+    if(apds.readAmbientLight (lightAmbient))
+    {                  
+        //Serial.println((String) "Ambient=" + lightAmbient); 
+    }
+    else
+    {
+        //Serial.println("Read light ERROR!");
+    }
+
+    if (Serial.available() > 0) 
+    {
+        String data = Serial.readStringUntil('\n');
+        Serial.print("You sent me: ");
+        if (data == getRasberiReqest)
+        {
+            apds.readAmbientLight (lightAmbient);
+            Serial.println("{\"P\":\"" + String(getPressure()) +
+              "\", \"lightAmbient\":\"" + String(lightAmbient) +
+              "\"}");
+        }
+        
+  }
 }
